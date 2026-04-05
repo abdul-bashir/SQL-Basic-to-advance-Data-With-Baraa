c5d7e7527269fff9f42d3b47ae9026822d883d24
MAB15 <basir20basir@gmail.com>
2026-04-05 18:52:52 +0600

    5.4.26 dokan

---
+++ practice query's/5.4.26.sql
@@ -0,0 +1,5 @@
+UPDATE persons
+SET phone='01712548567'
+WHERE id=1
+
+SELECT * FROM persons
---
+++ practice query's/Insert data from 'customers'table into 'persons' table.sql
@@ -0,0 +1,9 @@
+-- Insert data from 'customers'table into 'persons' table--
+INSERT INTO persons (id, person_name,birth_date,phone)
+SELECT 
+id,
+first_name,
+NULL,
+'Unknown'
+FROM customers
+
